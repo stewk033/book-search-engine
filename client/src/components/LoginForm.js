8dae98dcc33f48d474b91cc6af162e94e3535553
stewk033 <kevin.c.stewart04@gmail.com>
2022-02-04 21:31:41 -0500

    rebuild react

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -8,7 +8,7 @@ const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: "", password: "" });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
-  const [login, { error }] = useMutation(LOGIN_USER);
+  const [loginUser, { error }] = useMutation(LOGIN_USER);
 
   useEffect(() => {
     if (error) {
